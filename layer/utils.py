549d26f30f140436ccfec709c7ed5000843b3df6
Archit Dubey <pokemongochamp47@gmail.com>
2023-11-27 01:23:12 +0530

    Data Migration script and data dictionary.

--- layer/utils.py
+++ layer/utils.py
@@ -5,79 +5,57 @@ import json
 
 
 
-def migrate_indicators(filename="layer/IDS-DRR Model Indicators | Data Dictionary - data_dictionary.csv"):
+def migrate_indicators(filename="layer/ids_drr_data_dict.csv"):
     df = pd.read_csv(filename)
-    print(df.shape)
-    print(df.columns)
-    # df = df.drop(columns=['category'])
-    # indicators = indicator_df.columns
-    # print(indicators)
+    # print(df.shape)
+    # print(df.columns)
+    
     for row in df.itertuples(index=False):
         try:
             print("Processing Indicator -", row.indicatorSlug)
             try:
-                Indicators.objects.get(name=row.indicatorTitle)
+                Indicators.objects.get(slug=row.indicatorSlug.lower())
                 print("Already Exists!")
             except Indicators.DoesNotExist:
                 print("Processing Unit -", row.unit)
-                try:
-                    unit_obj = Unit.objects.get(name=row.unit.lower())
-                    print(f"Hey! Unit {unit_obj.name} already exists!")
-                except Unit.DoesNotExist:
-                    unit_obj = Unit(name=row.unit.lower())
-                    unit_obj.save()
-                    print(f"Saved {unit_obj.name} to DB!")
+                if row.unit and not isinstance(row.unit, float):
+                    try:
+                        unit_obj = Unit.objects.get(name=row.unit.lower())
+                        print(f"Hey! Unit {unit_obj.name} already exists!")
+                    except Unit.DoesNotExist:
+                        unit_obj = Unit(name=row.unit.lower())
+                        unit_obj.save()
+                        print(f"Saved {unit_obj.name} to DB!")
+                else:
+                    print("Skipping Indicator as no Unit was provided!")
                 parent_obj = None
                 try:
-                    if row.indicatorCategory:
-                        parent_obj = Indicators.objects.get(name=row.indicatorCategory)
+                    if row.parent and not isinstance(row.parent, float):
+                        parent_obj = Indicators.objects.get(name=row.parent.strip())
                     else:
                         pass
                 except Indicators.DoesNotExist:
-                    print(f"Failed to get the parent indicator for {row.indicatorSlug}")
-            #                 continue
-            #     parent_obj = None
-            #     if row.indicatorCategory and row.indicatorTitle != 'Composite Score':
-            #         # print(row.indicatorCategory)
-            #         indicator = row.indicatorCategory.split(" ")
-            #         # print(indicator)
-            #         if indicator[-1] == "Indicators":
-            #             indicator.pop()
-            #             x = ""
-            #             for indc in indicator:
-            #                 x = x+indc+" "
-            #             print(x)
-            #             try:
-            #                 parent_obj = Indicators.objects.get(name=x.strip())
-            #             except Indicators.DoesNotExist:
-            #                 print(f"Failed to get the parent indicator for {row.indicatorTitle}")
-            #                 continue
-            #         else:
-            #             try:
-            #                 parent_obj = Indicators.objects.get(name=row.indicatorCategory)
-            #             except Indicators.DoesNotExist:
-            #                 print(f"Failed to get the parent indicator for {row.indicatorCategory}")
-            #                 continue
+                    print(f"Failed to get the parent indicator for {row.indicatorSlug.lower()}")
+            
                 indicator_obj = Indicators(
-                    name = row.indicatorTitle,
-                    slug = row.indicatorSlug.lower() if row.indicatorSlug else None,
-                    long_description = row.indicatorDescription if row.indicatorDescription else None,
+                    name = row.indicatorTitle.strip(),
+                    slug = row.indicatorSlug.lower().strip() if row.indicatorSlug else None,
+                    long_description = row.indicatorDescription.strip() if row.indicatorDescription else None,
                     # short_description = row.indicatorDescription if row.indicatorDescription else None,
-                    category = row.indicatorCategory if row.indicatorCategory else None,
+                    category = row.indicatorCategory.strip() if row.indicatorCategory else None,
                     # type = row.indicatorType if row.indicatorType else None
                     unit = unit_obj,
-                    data_source = row.dataSource if row.dataSource else None,
+                    data_source = row.dataSource.strip() if row.dataSource else None,
                     parent=parent_obj,
                     is_visible = True if row.visible == 'y' else False,
                 )
                 indicator_obj.save()
                 print("Added indicator to the database.")
+            print('---------------------------')
         except Exception as e:
             print("Process Failed with error -", e)
             
-    # rint(df)
-    
-    
+
 
 def migrate_geojson(filename="layer/assam_revenue_circles_nov2022_4326.geojson"):
     
@@ -130,8 +108,8 @@ def migrate_data(filename="layer/MASTER_DATA_FRONTEND_2022onwards.csv"):
     df = pd.read_csv(filename)
     print(df.shape)
     # print(df.columns)
-    df = df.drop(columns=['district', 'rc_area', 'year', 'month', 'rank', 'topsis-score'])
-    data_columns = [cols.lower().replace("_", "-") for cols in df.columns]
+    # df = df.drop(columns=['district', 'rc_area', 'year', 'month', 'rank', 'topsis-score'])
+    # data_columns = [cols for cols in df.columns] #.lower().replace("_", "-") 
     # print(data_columns)
     
     # Get all columns from DB.
@@ -149,11 +127,6 @@ def migrate_data(filename="layer/MASTER_DATA_FRONTEND_2022onwards.csv"):
             print(e)
         # for indc in reqd_columns:
         for indc_obj in reqd_columns:
-            # if indc in data_columns:
-            # try:
-            #     indicator_obj = Indicators.objects.get(slug=indc)
-            # except Exception as e:
-            #     print(e)
             print(f"Adding data for RC - {geography_obj.name}")
             print(f"Adding data for Indicator - {indc_obj.slug}")
             # name = indc_obj
